154f60c802bc9c9be96fac4e07a3c8352835f806
Farhan Akhtar <farhandx1@hotmail.com>
2023-01-21 12:54:18 +0800

    started project

---
+++ src/main/java/com/dietApp/project/ProjectApplication.java
@@ -0,0 +1,21 @@
+package com.dietApp.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+
+	}
+
+
+
+
+}
